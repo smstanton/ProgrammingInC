b1a0b9ced07063e9abdf32360e86d2d335500996
Stephen Stanton <stephen.stanton@gmail.com>
2019-06-04 06:30:12 -0600

    Complete chapter 3

---
+++ e3.4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+{
+  int value1, value2, sum;
+  value1 = 15;
+  value2 = 87;
+  sum = value2 - value1;
+
+  printf ("%i minus %i equals %i\n", value2, value1, sum);
+
+  return 0;
+}
